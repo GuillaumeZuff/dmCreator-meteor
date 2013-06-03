87fea1dcdf663a67556a1beb46f30a1f15a103a5
GuillaumeZuff <guillaume.zufferey@gmail.com>
2013-06-03 14:16:17 +0200

    initial commit

---
+++ dmCreator.js
@@ -0,0 +1,13 @@
+var dm = Npm.require('dmCreator');
+
+DmCreator = {
+    // options:
+    // - path: output directory (MUST exist)
+    // - data: the text to encode in the datamatrix
+    // returns:
+    // - true if successful, false otherwise
+    generateDm: function(options) {
+        if (options === undefined) options = {}; 
+        return dm.generateDm(options);
+    }
+};
---
+++ package.js
@@ -0,0 +1,9 @@
+Package.describe({
+    summary: "Generate datamatrix tags (server side)."
+});
+
+Npm.depends({dmcreator: '0.0.1'});
+
+Package.on_use(function(api) {
+    api.add_files('dmCreator.js', 'server');
+});
