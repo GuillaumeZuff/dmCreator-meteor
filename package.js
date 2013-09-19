f2b4476174da949981e2eab26bf5dae17b346f43
GuillaumeZuff <guillaume.zufferey@gmail.com>
2013-09-19 14:02:05 +0200

    Corrected bad dependency version.

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
     summary: "Generate datamatrix tags (server side)."
 });
 
-Npm.depends({dmcreator: "latest"});
+Npm.depends({dmcreator: "0.0.7"});
 
 Package.on_use(function(api) {
     api.add_files("dmCreator.js", "server");
