df541913494fb64d7080ed52ad1d863c7287aab8
GuillaumeZuff <guillaume.zufferey@gmail.com>
2013-09-23 14:29:30 +0200

    increased version number (dmCreator updated)

--- dmCreator.js
+++ dmCreator.js
@@ -2,10 +2,13 @@ var dm = Npm.require('dmcreator');
 
 DmCreator = {
     // options:
-    // - path: output directory (MUST exist)
     // - data: the text to encode in the datamatrix
     // returns:
-    // - true if successful, false otherwise
+    // - success (bool)
+    // - width (width of image)
+    // - height
+    // - channels
+    // - pixels (array containing pixel values)
     generateDm: function(options) {
         if (options === undefined) options = {}; 
         return dm.generateDm(options);
