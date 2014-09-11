43961479832fe002a53d794bb22bd3b04f4b460c
GuillaumeZuff <guillaume.zufferey@gmail.com>
2014-09-11 20:25:49 +0200

    updated for meteor 0.9.x

--- package.js
+++ package.js
@@ -1,12 +1,13 @@
 Package.describe({
+    name: "dmcreator",
+    version: "0.0.1",
     summary: "Generate datamatrix tags (server side)."
 });
 
 Npm.depends({dmcreator: "0.7.5"});
 
-Package.on_use(function(api) {
-    api.add_files("dmCreator.js", "server");
-    if (typeof api.export !== 'undefined') {
-        api.export(['DmCreator'],'server');
-    }
+Package.onUse(function(api) {
+    api.addFiles("dmCreator.js", "server");
+
+    api.export(['DmCreator'],'server');
 });
